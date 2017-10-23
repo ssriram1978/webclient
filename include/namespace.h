bc6c70e39b55c2fdd0d422f3ab4dfb998208af6c
Sriram Sridhar <sriram.sridhar@verizon.com>
2017-10-23 10:15:17 -0400

    Checked in namespace.h

---
+++ include/namespace.h
@@ -0,0 +1,20 @@
+#ifndef NAMESPACE_H
+#define NAMESPACE_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+namespace webclient {
+
+using namespace std;
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+}
+
+#endif
+
